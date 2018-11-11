ec9390ab01561ab952eced82e4a94b81322d35e1
Marlin <marlinverhulst@gmail.com>
2018-11-11 20:47:57 +0000

    photo_id colmn added to users table file upload added

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -4,7 +4,7 @@
 
     <h1>Create Users</h1>
 
-    {!! Form::open(['method'=='POST', 'action'=> 'AdminUsersController@store'])!!}
+    {!! Form::open(['method'=='POST', 'action'=> 'AdminUsersController@store', 'files'=>true])!!}
         <div class="form-group">
             {!! Form::label('name', 'Name:')!!}
             {!!Form::text('name', null, ['class'=> 'form-control']) !!}
@@ -18,8 +18,12 @@
             {!! Form::select('role_id', ['' => 'Choose Options'] + $roles, null, ['class'=> 'form-control']) !!}
         </div>
         <div class="form-group">
-            {!! Form::label('status', 'Status:')!!}
-            {!! Form::select('status', array(1 => 'Active', 0 => 'Disabled'), 0, ['class'=> 'form-control']) !!}
+            {!! Form::label('is_active', 'Status:')!!}
+            {!! Form::select('is_active', array(1 => 'Active', 0 => 'Disabled'), 0, ['class'=> 'form-control']) !!}
+        </div>
+        <div class="form-group">
+            {!! Form::label('file', 'Upload File:')!!}
+            {!! Form::file('file', null, ['class'=> 'form-control']) !!}
         </div>
         <div class="form-group">
             {!! Form::label('password', 'Password:')!!}
@@ -31,6 +35,8 @@
             {!!Form::submit('Create User', ['class'=> 'btn btn-primary']) !!}
         </div>
     {!!Form::close()!!}  
+
+    @include('includes.form_error')
     
 
     
