90e1e09d1584b69ac806f7e7959a33bb342a9468
Marlin <marlinverhulst@gmail.com>
2018-11-22 19:03:28 +0000

    Create post page completed

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -8,6 +8,7 @@ use App\Http\Requests;
 use App\Http\Requests\PostCreateRequest;
 use App\Post;
 use App\Photo;
+use App\Category;
 use Illuminate\Support\Facades\Auth;
 
 class AdminPostsController extends Controller
@@ -32,7 +33,9 @@ class AdminPostsController extends Controller
     public function create()
     {
         //
-        return view('admin.posts.create');
+        $categories = Category::lists('name','id')->all();
+
+        return view('admin.posts.create', compact('categories'));
     }
 
     /**
